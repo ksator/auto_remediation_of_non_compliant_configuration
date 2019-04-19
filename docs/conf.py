b5415ffbece30b7650ee18f27019b667af870216
Your Name <you@example.com>
2019-04-19 08:47:44 -0700

    RTD update

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -37,8 +37,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
-
-extensions=['recommonmark']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
